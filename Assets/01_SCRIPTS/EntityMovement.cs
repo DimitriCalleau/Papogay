2172951fcb46678a73a93fbc68b6788ae5f08936
estebanluginbuhl <estebanluginbuhl@gmail.com>
2021-01-26 16:04:57 +0100

    Feature-entity_base+scripts_manquants

---
+++ Assets/01_SCRIPTS/EntityMovement.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EntityMovement : Entity
+{
+    void Update()
+    {
+        Move(moveSpeed, EntityStatus.Enemy, PlayerInRange(playerDetectionRadius, GameManager.Instance.player.transform.position));
+    }
+
+    public void Move(float moveSpeed, EntityStatus status, bool attacksPlayer)
+    {
+        
+    }
+
+    public bool PlayerInRange(float detectionDistance, Vector3 playerPosition)
+    {
+        if (Vector3.Distance(transform.position, playerPosition) <= detectionDistance)
+        {
+            return true;
+        }
+        else
+            return false;
+    }
+}
