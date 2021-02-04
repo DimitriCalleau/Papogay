5674be2a818dcc88059b5cda903dd0fd89df6286
estebanluginbuhl <estebanluginbuhl@gmail.com>
2021-02-04 16:35:53 +0100

    Update PreviewBait.cs

---
+++ Assets/01_SCRIPTS/PreviewBait.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PreviewBait
+{
+    public Mesh mesh;
+    public Vector3 locationPosition, boxRange;
+    public int rotation;
+    public float sphereRange;
+
+
+}
