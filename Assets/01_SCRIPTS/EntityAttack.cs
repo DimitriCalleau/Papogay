f2de2f66d6382acf2d1632509367ed37fa87bbbf
dimitricalleau <dimitri.calleau@outlook.fr>
2021-03-04 19:22:20 +0100

    Integration_Anim_Test

--- Assets/01_SCRIPTS/EntityAttack.cs
+++ Assets/01_SCRIPTS/EntityAttack.cs
@@ -8,8 +8,8 @@ public class EntityAttack : MonoBehaviour
     public Entity entity;
     float timerCooldownAttack, timerDamageAnticipation;
     public int allyAttackDamages, enemyAttackDamages; 
-    public float entityDamageCooldown, entityAttackRange, entityStratAttackingRange, damageAnticipationDuration;
-    bool isAttacking;
+    public float entityDamageCooldown, entityAttackRange, entityStratAttackingRange, enemyDamageAnticipationDuration, allyDamageAnticipationDuration;
+    bool enemyIsAttacking, allyIsAttacking;
     public LayerMask allyAttackLayer = -1;
     void Update()
     {
@@ -28,15 +28,15 @@ public class EntityAttack : MonoBehaviour
                         {
                             entity.anm.SetTrigger("Attack");
                             timerCooldownAttack = entityDamageCooldown;
-                            timerDamageAnticipation = damageAnticipationDuration;
-                            isAttacking = true;
+                            timerDamageAnticipation = enemyDamageAnticipationDuration;
+                            enemyIsAttacking = true;
                         }
                         else
                         {
                             entity.anm.SetTrigger("Attack");
                             timerCooldownAttack = entityDamageCooldown;
-                            timerDamageAnticipation = damageAnticipationDuration;
-                            isAttacking = true;
+                            timerDamageAnticipation = enemyDamageAnticipationDuration;
+                            enemyIsAttacking = true;
                         }
                     }
 
@@ -48,19 +48,19 @@ public class EntityAttack : MonoBehaviour
                 }
                 else
                 {
-                    if (isAttacking == true)
+                    if (enemyIsAttacking == true)
                     {
                         if (entity.target != null && Vector3.Distance(transform.position, entity.target.transform.position) <= entityAttackRange)
                         {
                             if (entity.target == GameManager.Instance.player)
                             {
                                 GameManager.Instance.playerStats.DamagePlayer(enemyAttackDamages);
-                                isAttacking = false;
+                                enemyIsAttacking = false;
                             }
                             else
                             {
                                 entity.target.GetComponent<Entity>().DamageEntity(enemyAttackDamages, false);
-                                isAttacking = false;
+                                enemyIsAttacking = false;
                             }
                         }
                     }
@@ -68,15 +68,37 @@ public class EntityAttack : MonoBehaviour
                 break;
 
             case EntityStatus.Ally:
+
+                Collider[] firme = Physics.OverlapSphere(transform.position + Vector3.up, entityAttackRange, allyAttackLayer);
+
                 if (entity.target != null)//link between destination and target
                 {
-                    Collider[] firme = Physics.OverlapSphere(transform.position + Vector3.up, entityAttackRange, allyAttackLayer);
                     if(firme.Length != 0)
                     {
                         for (int i = 0; i < firme.Length; i++)
                         {
-                            entity.Dead();
-                            firme[i].GetComponent<Firme>().DamageFirme(allyAttackDamages);
+                            entity.anm.SetTrigger("Attack");
+                            timerDamageAnticipation = allyDamageAnticipationDuration;
+                            allyIsAttacking = true;
+                        }
+                    }
+                    if (timerDamageAnticipation >= 0)
+                    {
+                        timerDamageAnticipation -= Time.deltaTime;
+                    }
+                    else
+                    {
+                        if (allyIsAttacking == true)
+                        {
+                            if (firme.Length != 0)
+                            {
+                                for (int i = 0; i < firme.Length; i++)
+                                {
+                                    firme[i].GetComponent<Firme>().DamageFirme(allyAttackDamages);
+                                    allyIsAttacking = false;
+                                    entity.Dead();
+                                }
+                            }
                         }
                     }
                 }
