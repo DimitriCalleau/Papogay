925809840ac4722dff1d70653b80250eb28333ee
Esteban Luginbuhl <estebanluginbuhl@gmail.com>
2021-03-08 13:17:56 +0100

    push-tech_part_of_FX

---
+++ Assets/01_SCRIPTS/V_S_FX_Library.cs
@@ -0,0 +1,171 @@
+﻿using UnityEngine;
+
+public class V_S_FX_Library : MonoBehaviour
+{
+    [Header("Sound FX")]
+    public AudioSource S_enmSpawn;
+    public AudioSource S_endWave, S_startWave, S_cantSummonTrap, S_shop, S_enmAttack, S_dash, S_trapActivated, S_trapDestroyed, S_trapPlaced, S_playerDeath, S_conversion, S_newZoneOnMap;
+    [Header("Visual FX")]
+    public ParticleSystem V_corpoSpawn;
+    public ParticleSystem V_spawnEnm, V_placeTrap, V_convertToAlly, V_convertToEnm, V_convertToNeutral, V_DamagedCorpo;
+    public int interationParticleSpawnEnm;
+    public GameObject V_shopBeam, V_corpoBeam, V_playerDamaged, V_entityDamaged;
+
+    #region SFX
+
+    public void SpawnENM()
+    {
+        //V_enmSpawn.Play();
+        S_enmSpawn.Play();
+    }
+
+    public void EndWave()
+    {
+        //V_endWave.Play();
+        S_endWave.Play();
+    }
+
+    public void StartWave()
+    {
+        //V_startWave.Play();
+        S_startWave.Play();
+    }
+
+    public void CantSummonTrap()
+    {
+        //V_cantSummonTrap.Play();
+        S_cantSummonTrap.Play();
+    }
+
+    public void Shop()
+    {
+        //V_shop.Play();
+        S_shop.Play();
+    }
+
+    public void EnmAttack()
+    {
+        //V_enmAttack.Play();
+        S_enmAttack.Play();
+    }
+
+    public void Dash()
+    {
+        //V_dash.Play();
+        S_dash.Play();
+    }
+
+    public void TrapOn()
+    {
+        //V_trapActivated.Play();
+        S_trapActivated.Play();
+    }
+
+    public void TrapOff()
+    {
+        //V_trapDestroyed.Play();
+        S_trapDestroyed.Play();
+    }
+
+    public void PlaceTrap()
+    {
+        //V_trapPlaced.Play();
+        S_trapPlaced.Play();
+    }
+
+    public void PlayerDeath()
+    {
+        //V_playerDeath.Play();
+        S_playerDeath.Play();
+    }
+
+    public void Conversion()
+    {
+        //V_conversion.Play();
+        S_conversion.Play();
+    }
+
+    public void NewZoneAvailable()
+    {
+        //V_newZoneOnMap.Play();
+        S_newZoneOnMap.Play();
+    }
+
+    #endregion
+
+    #region VFX
+    
+    private float timerFloat;
+
+    void FixedUpdate()
+    {
+        timerFloat -= Time.deltaTime;
+        if (timerFloat <= 0)
+        {
+            timerFloat = 5;
+            V_corpoBeam.SetActive(false);
+            V_shopBeam.SetActive(false);
+            V_playerDamaged.SetActive(false);
+            V_entityDamaged.SetActive(false);
+        }
+    }
+
+    public void SpawnCorpoFX()
+    {
+        V_corpoSpawn.Play();
+    }
+
+    public void ShopBeam()
+    {
+        V_shopBeam.SetActive(true);
+    }
+
+    public void CorpoBeam()
+    {
+        V_corpoBeam.SetActive(true);
+    }
+
+    public void SpawnEnmFX()
+    {
+        for (int i = 0; i < interationParticleSpawnEnm; i++)
+        {
+            V_spawnEnm.Play();
+        }
+    }
+
+    public void PlayerDamaged()
+    {
+        V_playerDamaged.SetActive(true);
+    }
+
+    public void EntityDamaged()
+    {
+        V_entityDamaged.SetActive(true);
+    }
+
+    public void PlaceTrapFX()
+    {
+        V_placeTrap.Play();
+    }
+
+    public void ConvertToAlly()
+    {
+        V_convertToAlly.Play();
+    }
+
+    public void ConvertToEnm()
+    {
+        V_convertToEnm.Play();
+    }
+
+    public void ConvertToNeutral()
+    {
+        V_convertToNeutral.Play();
+    }
+
+    public void CorporationDamaged()
+    {
+        V_DamagedCorpo.Play();
+    }
+    #endregion
+}
