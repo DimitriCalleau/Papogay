cff3cf1563705f9632791106a6d3389be97bbe37
estebanluginbuhl <estebanluginbuhl@gmail.com>
2021-03-18 18:17:17 +0100

    update shader

---
+++ Assets/04_POSTPROCESS/SeeThrough/SeeThrough/ScriptedSeeThrough/SeeThroughAdaptation.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SeeThroughAdaptation : MonoBehaviour
+{
+    public static int shaderPosID = Shader.PropertyToID("_Position");
+    public static int shaderSizeID = Shader.PropertyToID("_Size");
+
+    Material goMaterial;
+    public Camera mainCamera;
+    GameObject seeThroughSphere;
+    public LayerMask seeThroughLayer;
+
+    void Update()
+    {
+        RaycastHit raycastHit;
+
+        if (Physics.Raycast(mainCamera.transform.position, (transform.position - mainCamera.transform.position).normalized, out raycastHit, Mathf.Infinity, seeThroughLayer))
+        {
+            if (raycastHit.collider.gameObject)
+            {
+                goMaterial = raycastHit.collider.gameObject.GetComponent<MeshRenderer>().material;
+                goMaterial.SetFloat(shaderSizeID, 1.5f);
+            }
+            else
+            {
+                goMaterial = raycastHit.collider.gameObject.GetComponent<MeshRenderer>().material;
+                goMaterial.SetFloat(shaderSizeID, 0);
+            }
+        }
+
+        var view = mainCamera.WorldToViewportPoint(transform.position);
+        goMaterial.SetVector(shaderPosID, view);
+    }
+}
---
+++ Assets/04_POSTPROCESS/SeeThrough/SeeThrough/byGraph/SeeThroughAlignement.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SeeThroughAlignement : MonoBehaviour
+{
+    public static int shaderPosID = Shader.PropertyToID("_Position");
+    public static int shaderSizeID = Shader.PropertyToID("_Size");
+
+    public Material mat;
+    public Camera _camera;
+    public LayerMask rayMaskLayer;
+    RaycastHit raycastHit;
+
+    void Update()
+    {
+        var dir = _camera.transform.position - transform.position;
+        var ray = new Ray(transform.position, dir.normalized);
+        
+        //if (Physics.Raycast(ray, Mathf.Infinity, rayMaskLayer))
+        if (Physics.Raycast(_camera.transform.position, (transform.position - _camera.transform.position).normalized, Mathf.Infinity, rayMaskLayer))
+        {
+            mat.SetFloat(shaderSizeID, 1.5f);
+        }
+        else
+        {
+            mat.SetFloat(shaderSizeID, 0);
+        }
+
+        var view = _camera.WorldToViewportPoint(transform.position);
+        mat.SetVector(shaderPosID, view);
+    }
+}
